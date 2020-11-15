c9479f0559d7ea1b81281b8e20487325f8d207a9
matthlud <muster.mann123@t-online.de>
2020-11-16 00:09:56 +0100

    Button Test Completed

--- app/src/main/java/com/pixel/arbitartwork/MainActivity.kt
+++ app/src/main/java/com/pixel/arbitartwork/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.pixel.arbitartwork
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         /* setSupportActionBar(findViewById(R.id.toolbar)) */
-
+        val rollButtonI = findViewById<Button>(R.id.rollButton);
+        val textViewI = findViewById<TextView>(R.id.textView);
+        rollButtonI.setOnClickListener {
+            textViewI.text="Yay! I am a Kotlin beginner"
+        }
     }
 
 
