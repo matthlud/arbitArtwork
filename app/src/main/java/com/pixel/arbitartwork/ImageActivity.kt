d707c8cde3467357f77cf5fe97843824f5ea5bb2
matthlud <muster.mann123@t-online.de>
2020-11-15 01:07:49 +0100

    Initial Commit

---
+++ app/src/main/java/com/pixel/arbitartwork/ImageActivity.kt
@@ -0,0 +1,10 @@
+package com.pixel.arbitartwork
+
+class ImageActivity {
+
+//    private val img = Image("/images/photo.jpg")
+//    private val wImg1 = WritableImage(img.pixelReader, img.width.toInt(), img1.height.toInt())
+//
+//    private val width = 400
+//    private val height = 400
+}
---
+++ app/src/main/java/com/pixel/arbitartwork/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.pixel.arbitartwork
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /* setSupportActionBar(findViewById(R.id.toolbar)) */
+
+    }
+
+
+    fun onRollClick(view:View){
+
+    }
+
+}
